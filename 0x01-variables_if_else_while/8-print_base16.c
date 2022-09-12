bf275a691f67cea64f4ae650f9f8acd3e84883db
Ofuyo <vikmunala@gmail.com>
2022-09-12 06:16:10 -0700

    variables

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * return: (0)
+ *
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9' ; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'a'; c <= 'f' ; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
