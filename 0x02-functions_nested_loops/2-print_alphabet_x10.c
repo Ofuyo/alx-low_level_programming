e0a78e45b0a577e13b9e7ff48b617400f5a989d6
Ofuyo <vikmunala@gmail.com>
2022-09-13 23:01:19 -0700

    alphabet x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,8 @@
 #include "main.h"
-
 /**
  * print_alphabet_x10 - function to print abc 10 times
  *
  * Return: 0
- *
  */
 void print_alphabet_x10(void)
 {
@@ -19,4 +17,3 @@ void print_alphabet_x10(void)
 		_putchar('\n');
 	}
 }
-
