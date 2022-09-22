38d3136e893d12467384b1abc740fe87c940a323
Ofuyo <vikmunala@gmail.com>
2022-09-22 15:24:14 -0700

    string copy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,12 +1,11 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * _strncpy - c function that copies a string,include the terminating null byte
  * @dest: buffer storing the string copy
  * @src: the source string
  * @n: max number of bytes copied
- * Returns: returns
+ * Return: returns
  */
 
 char *_strncpy(char *dest, char *src, int n)
