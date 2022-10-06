bba3fd65bfd4674681bb31ef253e27eaf2581765
Ofuyo <vikmunala@gmail.com>
2022-10-06 06:25:35 -0700

    more malloc, free tasks

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * array_range - creates an array of integers
+ * @min: first value of array
+ * @max: last value of array
+ *
+ * Return: if min > max of func fails - NULL
+ *	Otherwise - pointer to newly created array
+ */
+
+int *array_range(int min, int max)
+{
+	int *array, index, size;
+
+	if (min > max)
+		return (NULL);
+
+	size = max - min + 1;
+
+	array = malloc(sizeof(int) * size);
+
+	if (array == NULL)
+		return (NULL);
+
+	for (index = 0; index < size; index++)
+		array[index] = min++;
+
+	return (array);
+}
